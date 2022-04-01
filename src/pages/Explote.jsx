1260436eb749d5e944dc3ffb49e7e3699f2619ef
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-04-01 11:21:31 -0400

    Explore Page

--- src/pages/Explote.jsx
+++ src/pages/Explote.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
 function Explote() {
-  return <div>Explote</div>;
+  return (
+    <div className="explore">
+      <header className="pageHeader">Explore</header>
+      <main>{/*Slider */}</main>
+      <p className="exploreCategoryHeading">Categories</p>
+      <div className="exploreCategories">
+        <Link to="/category/rent">
+          <img
+            src={rentCategoryImage}
+            alt="rent"
+            className="exploreCategoryImg"
+          />
+          <p className="exploreCategoryName">Places for rent</p>
+        </Link>
+        <Link to="/category/sell">
+          <img
+            src={sellCategoryImage}
+            alt="rent"
+            className="exploreCategoryImg"
+          />
+          <p className="exploreCategoryName">Places for sell</p>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Explote;
