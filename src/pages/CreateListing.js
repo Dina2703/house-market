712ddf2b539d60f9b4e07188bc24b576e4b7c815
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-04-04 22:22:45 -0400

    Get coords with Google Geocoding API part-2. setup logic for fetched data from the google API

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -61,8 +61,8 @@ function CreateListing() {
   const onSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    console.log(formData);
-    console.log(discountedPrice, regularPrice);
+    // console.log(formData);
+
     if (+discountedPrice >= +regularPrice) {
       setLoading(false);
       toast.error("Discounted price needs to be less than regular price");
@@ -84,10 +84,22 @@ function CreateListing() {
     if (geolocationEnabled) {
       //if geolocation is enabled, make a request to Google, value for the key, i got from console.cloud.google.com
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyCjzE5wcY6Q9yCpLasWZUks-r32Zghn7LY`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
       );
       const data = await response.json();
       console.log(data);
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+      location =
+        data.status === "ZERO_RESULTS"
+          ? undefined
+          : data.results[0]?.formatted_address;
+      if (location === undefined || location.includes("undefined")) {
+        setLoading(false);
+        toast.error("Please, enter a correct address");
+        return;
+      }
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
